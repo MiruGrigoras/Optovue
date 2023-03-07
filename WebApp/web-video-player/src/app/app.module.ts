e0e1746ad4a0b475a8d7d5b5bfedc3c78fa59d38
claudiabudimir <claudiabudimir1354@gmail.com>
2023-03-07 21:49:33 +0100

    feat: create video component

--- WebApp/web-video-player/src/app/app.module.ts
+++ WebApp/web-video-player/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoComponent } from './video/video.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
