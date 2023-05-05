ff9138f83d210baea7af317d9f1a1ece79ff0165
MiruGrigoras <miruna.grig@gmail.com>
2023-05-05 15:48:53 +0200

    Get all processes from db with Sequelize

--- OptovueServer/src/Sequelize/process/process.controller.ts
+++ OptovueServer/src/Sequelize/process/process.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProcessService } from './process.service';
 
 @Controller('process')
@@ -6,12 +6,8 @@ export class ProcessController {
   constructor(private processService: ProcessService) {}
 
   @Get()
-  getProcesses() {
-    const processes = this.processService.findAll();
-    return JSON.stringify(processes) + 'PROCESS text';
-  }
-  @Post()
-  addElement() {
-    this.processService.addNew();
+  async getProcesses() {
+    const processes = await this.processService.findAll();
+    return processes;
   }
 }
--- OptovueServer/src/Sequelize/process/process.service.ts
+++ OptovueServer/src/Sequelize/process/process.service.ts
@@ -1,8 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { PROCESS_REPOSITORY } from 'src/constants';
 import { Process } from './process.entity';
-import { QueryTypes } from 'sequelize';
-import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ProcessService {
@@ -12,42 +10,9 @@ export class ProcessService {
   ) {}
 
   async findAll(): Promise<object[]> {
-    console.log(this.processesRepository.getTableName() + 'Empty??');
-    // const processesOLD = await this.processesRepository.findAll<BPAProcess>({
-    //   attributes: ['processid'],
-    // });
-    const processes = await this.processesRepository.sequelize.query(
-      'SELECT [processid] FROM [BluePrism].[dbo].[BPAProcess] AS [BPAProcess];',
-      { type: QueryTypes.SELECT },
-    );
-    return processes; //.map((process) => new ProcessDB(process));
-  }
-
-  async addNew() {
-    this.processesRepository.create({
-      processid: uuidv4(),
-
-      ProcessType: 'P',
-
-      name: 'name',
-
-      createdate: Date.parse('01 Jan 1970 00:00:00 GMT'),
-
-      createdby: uuidv4(),
-
-      lastmodifieddate: Date.parse('01 Jan 1950 00:00:00 GMT'),
-
-      lastmodifiedby: uuidv4(),
-
-      AttributeID: Math.floor(Math.random() * 10000),
-
-      runmode: 1,
-
-      sharedObject: true,
-
-      forceLiteralForm: true,
-
-      useLegacyNamespace: true,
+    const processes = await this.processesRepository.findAll<Process>({
+      attributes: ['processid'],
     });
+    return processes;
   }
 }
